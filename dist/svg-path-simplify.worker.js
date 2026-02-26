08bf656ecbf987b15ea7841e1a829ed950854d40
herrstrietzel <der@herrstrietzel.de>
2026-02-26 10:10:50 +0100

    web worker cleanup

--- dist/svg-path-simplify.worker.js
+++ dist/svg-path-simplify.worker.js
@@ -14,49 +14,14 @@ self.onmessage = (e) => {
             let item = fileStack[i]
             let {svg} = item;
             let simplified =  svgPathSimplify(svg, settings)
-            //resultArr.push(simplified)
-            //console.log('item', i,  simplified);
-
             fileStack[i].simplified=simplified
         }
 
         let result = fileStack
         self.postMessage({ result });
 
-        /*
-        //JSON.stringify(data, null, ' ')
-
-        let simplified = svgPathSimplify(data, settings)
-        result = simplified
-
-        */
-
     } catch (err) {
         self.postMessage({ error: err.message });
     }
 
 }
-
-
-
-/*
-self.onmessage = (e) => {
-    //let { bmp, options } = e.data;
-    let { data, settings } = e.data;
-
-    try {
-        //let result = JSON.stringify(settings, null, ' ')
-        let result
-        //JSON.stringify(data, null, ' ')
-
-        let simplified = svgPathSimplify(data, settings)
-        result = simplified
-
-        self.postMessage({ result });
-
-    } catch (err) {
-        self.postMessage({ error: err.message });
-    }
-
-}
-*/
